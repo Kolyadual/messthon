b6ebbd80acd758b4fa72fb979687c61e53b8d7dc
Kolyadual <kolyadual@gmail.com>
2026-03-20 20:49:37 +0300

    Add files via upload

--- systemr.sh
+++ systemr.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-pip3 install --break-system-packages fastapi uvicorn websockets sqlalchemy passlib websocket-client requests
+# Runget System Requirements Bash script. Created by Kolyadual in 2026.
+
+sudo apt update && sudo apt install -y \
+    git \
+    cmake \
+    build-essential \
+    qtbase5-dev \
+    qttools5-dev \
+    qttools5-dev-tools \
+    libqt5svg5-dev \
+    libfilteraudio-dev \
+    libopus-dev \
+    libvpx-dev \
+    libsodium-dev \
+    libtoxcore-dev \
+    pkg-config \
+    ninja-build \
+    python3 \
+    python-is-python3 \
+    python3-pip
+
+
